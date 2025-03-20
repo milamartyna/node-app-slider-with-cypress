f5949bf952813a1d7d9bc1f9770cf05d2ecec022
Martyna Mila <martynamila2001@gmail.com>
2025-03-20 10:21:04 +0100

    add hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,18 +1,39 @@
-describe('Swiper Gallery Test', function () {
+describe("Swiper Gallery Test", function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-button-next').click();
-    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.visit("http://localhost:3000");
+    cy.get(".swiper-button-next").click();
+    cy.get(".swiper-slide-active").should("contain", "United Kingdom");
   });
 });
 
-describe('Swiper Gallery Test', function () {
+describe("Swiper Gallery Test", function () {
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-button-next').click();
+    cy.visit("http://localhost:3000");
+    cy.get(".swiper-button-next").click();
     cy.wait(2000);
-    cy.get('.swiper-button-next').click({ force: true });
+    cy.get(".swiper-button-next").click({ force: true });
     cy.wait(2000);
-    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get(".swiper-slide-active").should("contain", "Paris");
+  });
+});
+
+describe("Swiper Gallery Test", function () {
+  it("Checks if all slides are correctly shown", function () {
+    cy.visit("http://localhost:3000");
+    cy.get(".swiper-button-next").click({ force: true });
+    cy.wait(2000);
+    cy.get(".swiper-slide-active")
+      .should("contain", "London")
+      .should("contain", "United Kingdom");
+    cy.get(".swiper-button-next").click({ force: true });
+    cy.wait(2000);
+    cy.get(".swiper-slide-active")
+      .should("contain", "Paris")
+      .should("contain", "France");
+    cy.get(".swiper-button-next").click({ force: true });
+    cy.wait(2000);
+    cy.get(".swiper-slide-active")
+      .should("contain", "Rome")
+      .should("contain", "Italy");
   });
 });
